a74b44beabefe714569ae01dbe87c1032b78675f
knoxxknox <knoxxknox@gmail.comgit config user.namegit config --global user.email knoxxknox@gmail.com>
2020-05-14 19:15:44 -0300

    Desafio Codenation modulo 1

---
+++ main.py
@@ -0,0 +1,130 @@
+from datetime import datetime
+from collections import OrderedDict
+
+records = [
+    {'source': '48-996355555', 'destination': '48-666666666', 'end': 1564610974, 'start': 1564610674},
+    {'source': '41-885633788', 'destination': '41-886383097', 'end': 1564506121, 'start': 1564504821},
+    {'source': '48-996383697', 'destination': '41-886383097', 'end': 1564630198, 'start': 1564629838},
+    {'source': '48-999999999', 'destination': '41-885633788', 'end': 1564697158, 'start': 1564696258},
+    {'source': '41-833333333', 'destination': '41-885633788', 'end': 1564707276, 'start': 1564704317},
+    {'source': '41-886383097', 'destination': '48-996384099', 'end': 1564505621, 'start': 1564504821},
+    {'source': '48-999999999', 'destination': '48-996383697', 'end': 1564505721, 'start': 1564504821},
+    {'source': '41-885633788', 'destination': '48-996384099', 'end': 1564505721, 'start': 1564504821},
+    {'source': '48-996355555', 'destination': '48-996383697', 'end': 1564505821, 'start': 1564504821},
+    {'source': '48-999999999', 'destination': '41-886383097', 'end': 1564610750, 'start': 1564610150},
+    {'source': '48-996383697', 'destination': '41-885633788', 'end': 1564505021, 'start': 1564504821},
+    {'source': '48-996383697', 'destination': '41-885633788', 'end': 1564627800, 'start': 1564626000}
+]
+
+final_result = {}
+FIXED_VALUE = 0.36
+VALUE_PER_MINUTE = 0.09
+FULL_VALUE = 960 * 0.09
+NIGHT_HOUR = 22
+DAY_HOUR = 6
+MINUTES_IN_AN_HOUR = 60
+DECIMAL_PLACES = 2
+
+def dict_check(source):
+    result = final_result.get(source)
+    return result
+
+def rule_1(source, end, start):
+    if start.minute < end.minute:
+        total_chamada = ((end.hour - start.hour) * MINUTES_IN_AN_HOUR) + (end.minute - start.minute)
+    else:
+        total_chamada = ((end.hour - start.hour) * MINUTES_IN_AN_HOUR) - (start.minute - end.minute)
+    price = (total_chamada * VALUE_PER_MINUTE) + FIXED_VALUE
+    currente_value = dict_check(source)
+    if currente_value != None:
+        price = price + currente_value 
+    resultado_parcial = {}
+    resultado_parcial[source] = price 
+    final_result.update(resultado_parcial) 
+       
+def rule_2(source, end, start):
+    total_chamada = ((NIGHT_HOUR - start.hour) * MINUTES_IN_AN_HOUR) - start.minute
+    price = (total_chamada * VALUE_PER_MINUTE) + FIXED_VALUE
+    currente_value = dict_check(source)
+    if currente_value != None:
+        price = price + currente_value 
+    
+    resultado_parcial = {}
+    resultado_parcial[source] = price 
+    final_result.update(resultado_parcial)  
+
+def rule_3(source, end, start):    
+    total_chamada = ((end.hour - DAY_HOUR) * MINUTES_IN_AN_HOUR) + end.minute
+    price = (total_chamada * VALUE_PER_MINUTE) + FIXED_VALUE    
+    currente_value = dict_check(source)
+    if currente_value != None:
+        price = price + currente_value   
+    resultado_parcial = {}
+    resultado_parcial[source] = price  
+    final_result.update(resultado_parcial)     
+
+def rule_4(source): 
+    price = FIXED_VALUE
+    currente_value = dict_check(source)
+    if currente_value != None:
+        price = price + currente_value
+    resultado_parcial = {}
+    resultado_parcial[source] = price 
+    final_result.update(resultado_parcial) 
+    
+def rule_5(source): 
+    price = FULL_VALUE + FIXED_VALUE
+    currente_value = dict_check(source)
+    if currente_value != None:
+        price = price + currente_value
+    resultado_parcial = {}
+    resultado_parcial[source] = price   
+    final_result.update(resultado_parcial)     
+
+def charging_rule(source, end, start):
+    if start.hour >= DAY_HOUR and start.hour < NIGHT_HOUR:
+        if (end.hour >= DAY_HOUR and end.hour < NIGHT_HOUR) or (end.hour == NIGHT_HOUR and end.minute == 0):
+            rule_1(source, end, start) 
+            pass 
+        else:
+            rule_2(source, end, start) 
+            pass           
+    else:
+        if (end.hour >= DAY_HOUR and end.hour < NIGHT_HOUR) or (end.hour == NIGHT_HOUR and end.minute == 0):
+            rule_3(source, end, start)   
+        else:
+            if (end.hour >= start.hour and end.hour < DAY_HOUR) or (end.hour == DAY_HOUR and end.minute == 0):
+                rule_4(source)             
+            elif start.hour < DAY_HOUR and end.hour >= NIGHT_HOUR:
+                rule_5(source)
+            else:
+                rule_4(source)
+                                          
+def finale():
+    dict_aux = {}
+    record = []             
+    ordered_result = OrderedDict(sorted(final_result.items(), key=lambda x: x[1]))
+    for elemento in ordered_result:
+        dict_aux['source'] = elemento
+        dict_aux['total'] = round(ordered_result.get(elemento), DECIMAL_PLACES)
+        record.append(dict_aux)
+        dict_aux = {}      
+    record = list(reversed(record))
+    print(record)            
+ 
+def classify_by_phone_number(records):  
+    for data in records:
+        source = data.get('source')
+        end = datetime.fromtimestamp(data.get('end'))
+        start = datetime.fromtimestamp(data.get('start'))
+        charging_rule(source, end, start)
+ 
+classify_by_phone_number(records)
+finale()
+
+
+
+    
+    
+    
+    
